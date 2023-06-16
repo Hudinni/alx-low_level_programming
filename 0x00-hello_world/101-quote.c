5b7df0dff2118614b0b004e7c1227cf4361fd2d4
Hudinni <odehoisaac@gmail.com>
2023-06-16 08:28:49 +0100

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - entry point
- * Return: always 1 (success)
+ * Return: 1 is ok
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
