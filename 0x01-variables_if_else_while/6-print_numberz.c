43bfa2f94acc6f0289b9d17a2801d550e55c84cd
Hudinni <odehoisaac@gmail.com>
2023-06-18 03:54:44 +0100

    Program that print base 10 numbers  between 0 & 9, followed by a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program to print base 10 numbers between 0 & 9
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	for (int n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
