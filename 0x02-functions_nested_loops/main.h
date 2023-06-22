278e6b213010dff63bf4c5bdd6770d2d4a8bd210
Hudinni <odehoisaac@gmail.com>
2023-06-22 20:13:00 +0100

    Write a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 #include <unistd.h>
 
 /**
@@ -7,7 +9,10 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+#endif
