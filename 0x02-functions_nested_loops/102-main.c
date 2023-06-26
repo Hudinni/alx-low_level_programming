d7ab45dc217e7616d0d3510cf1f393900fd32ced
Hudinni <odehoisaac@gmail.com>
2023-06-26 01:08:56 +0100

    a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.

---
+++ 0x02-functions_nested_loops/102-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @n: variable counter
+ * Description: program to print first 50 Fibonacci numbers
+ * Return: always 0
+ */
+void print_fibonacci(int n);
+
+int main(void)
+{
+	print_fibonacci(50);
+	return (0);
+}
