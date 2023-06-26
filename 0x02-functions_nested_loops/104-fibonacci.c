789904eebf25b265111c24eefff50b8221d681d8
Hudinni <odehoisaac@gmail.com>
2023-06-27 00:27:09 +0100

    program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,31 +5,25 @@
  * starting with 1 and 2
  * Return: always 0
  */
-#include <stdio.h>
-#include <string.h>
-
 int main(void)
 {
 	int n;
-	unsigned int fibo1 = 1;
-	unsigned int fibo2 = 2;
-	unsigned int fibo_next;
-	char fibo_string[1244] = "";
-	char temp_string[13];
+	unsigned int fib1 = 1;
+	unsigned int fib2 = 2;
+	unsigned int fibn;
 
-	sprintf(fibo_string, "%u, %u, ", fibo1, fibo2);
+	printf("%u, %u, ", fib1, fib2);
 	for (n = 3; n <= 98; n++)
 	{
-		fibo_next = fibo1 + fibo2;
-		sprintf(temp_string, "%012u", fibo_next);
-		strcat(fibo_string, temp_string);
+		fibn = fib1 + fib2;
+		printf("%u", fibn);
 		if (n < 98)
 		{
-			strcat(fibo_string, ", ");
+			printf(", ");
 		}
-		fibo1 = fibo2;
-		fibo2 = fibo_next;
+		fib1 = fib2;
+		fib2 = fibn;
 	}
-	printf("%s\n", fibo_string);
+	printf("\n");
 	return (0);
 }
